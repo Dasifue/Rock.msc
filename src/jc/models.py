200c3e2529f175a3f4586765bb7e915fb8a48987
Dasifue <dasifue@gmail.com>
2021-05-17 23:19:58 +0600

    dropdown and peron's info

--- src/jc/models.py
+++ src/jc/models.py
@@ -26,6 +26,7 @@ class Person(models.Model):
     last_name = models.CharField(max_length=50, verbose_name = 'фамилия')
     instrument = models.CharField(max_length=30, verbose_name = 'инструмент')
     band = models.ForeignKey(Band, on_delete=models.CASCADE)
+    info = models.TextField(max_length=2000, blank=True, null=True)
     def __str__(self):
         return f"{self.name}"
     class Meta:
--- src/jc/views.py
+++ src/jc/views.py
@@ -3,36 +3,38 @@ from django.shortcuts import render, redirect
 from .models import Person, Song, Band, Genre, Album, Comment
 from jc.forms import CreateCommentForm
 from django.conf import settings
+
 #   BANDS
+def get_core_html_data():
+    genres = Genre.objects.all()
+    bands = Band.objects.all()
+    return {'bands':bands, 'genres':genres}
+
 def all_bands(request):
     genres = Genre.objects.all()
     bands = Band.objects.all()
     songs = Song.objects.all()
-    return render(request, 'all_bands.html', {'bands':bands,'genres':genres, 'songs':songs})
+    return render(request, 'all_bands.html', {'dropdown': get_core_html_data(), 'songs':songs})
 
 def band_info(request, band_id):
     band = Band.objects.get(id = band_id)
-    members = Person.objects.filter(band=band_id)
-    albums = Album.objects.filter(band=band_id)
-    return render(request, 'band_info.html', {'band':band, 'albums':albums, 'members':members})
-
+    members = Person.objects.filter(band = band_id)   
+    albums = Album.objects.filter(band = band_id)
+    return render(request, 'band_info.html', {'band':band, 'albums':albums, 'members':members, 'dropdown': get_core_html_data()})
 
 #   SONGS
-
-
 def song_info(request, song_id):
     song_inf = Song.objects.filter(id=song_id)
-    return render(request, 'song_info.html', {'song_inf':song_inf})
+    return render(request, 'song_info.html', {'song_inf':song_inf, 'dropdown': get_core_html_data()})
 
 def song_filter(request, genre_id):
     songs = Song.objects.filter(genre = genre_id)
-    return render(request, 'song_filter.html', {'songs':songs})
+    return render(request, 'song_filter.html', {'songs':songs, 'dropdown': get_core_html_data()})
 
 def album_info(request, album_id):
     songs = Song.objects.filter(album = album_id)
     album = Album.objects.get(id=album_id)
-    return render(request, 'album_info.html', {'songs':songs, 'album':album})    
-
+    return render(request, 'album_info.html', {'songs':songs, 'album':album, 'dropdown': get_core_html_data()})    
 
 #Create comment
 def create_comment(request):
@@ -48,4 +50,6 @@ def create_comment(request):
             res = requests.get(settings.URL+f'{author}\n{text}')
             print(res)
             return redirect('jc:all_bands')
-    return render(request, 'create_comment.html', {'form':form})
+    return render(request, 'create_comment.html', {'form':form, 'dropdown': get_core_html_data() })
+
+

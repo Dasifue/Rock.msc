03c704d74ee955be9b04b196ab5c713f36c6287b
Dasifue <75379454+Dasifue@users.noreply.github.com>
2022-05-10 20:17:54 +0600

    Update settings.py

--- src/main/settings.py
+++ src/main/settings.py
@@ -75,15 +75,10 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'jc_data',
-        'USER': 'jc_user',
-        'PASSWORD' : '1234567890',
-        'HOST' : '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
